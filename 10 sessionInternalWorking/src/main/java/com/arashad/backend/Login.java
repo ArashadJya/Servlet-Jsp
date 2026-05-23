61a60b7a1ad9c14d6599eac7552805c647ca3494
ArashadJya <jyaarashad@gmail.com>
2026-05-23 11:35:11 +0530

    session-Internal-Working

---
+++ 10 sessionInternalWorking/src/main/java/com/arashad/backend/Login.java
@@ -0,0 +1,45 @@
+package com.arashad.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/loginform")
+public class Login extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		resp.setContentType("text/html");
+		
+		String email=req.getParameter("email1");
+		String password=req.getParameter("pass1");
+		
+		String name="Arashad Jya";   //it will reterive from database
+		
+		HttpSession session=req.getSession();
+		session.setAttribute("session_name", name);
+		
+		if(email.equals("jya@gmail.com")&& password.equals("jya@123") ) {
+			
+			RequestDispatcher rd=req.getRequestDispatcher("/profile");
+			rd.forward(req, resp);
+		}
+		else {
+			RequestDispatcher rd=req.getRequestDispatcher("error.html");
+			rd.include(req, resp);
+			
+			RequestDispatcher rd1=req.getRequestDispatcher("Login.html");
+			rd1.include(req, resp);
+			
+		}
+	}
+
+}
---
+++ 10 sessionInternalWorking/src/main/java/com/arashad/backend/profile.java
@@ -0,0 +1,62 @@
+package com.arashad.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
+
+@WebServlet("/profile")
+public class profile  extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+         PrintWriter out=resp.getWriter();
+         resp.setContentType("text/html");
+		
+		HttpSession session=req.getSession();
+		String name=(String)session.getAttribute("session_name");
+		
+		out.println(" <h3  style='color: green'>Name : "+name+"</h3>");
+		
+		out.println("<a href='home'>Home</a>"); 
+		out.println("  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+		
+		out.println("<a href='profile'>Profile</a>");
+		out.println("  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+		
+		out.println("<a href='contactUs'>Contact Us</a>");
+		out.println("  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+		
+		out.println("<a href='logout'>Logout</a>");
+		
+		
+		out.println("<br/><br/><br/>");
+		
+		out.println("<h3>Profile page</h3>");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		service(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		service(req, resp);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
