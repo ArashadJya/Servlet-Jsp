358fbc0dc59958f8fbb1ddaf6b11414de94f5816
ArashadJya <jyaarashad@gmail.com>
2026-05-26 00:49:44 +0530

    Tenth Commit

---
+++ 15 servletConfigDemo1/src/main/java/com/arashad/backend/myServlet.java
@@ -0,0 +1,42 @@
+package com.arashad.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class myServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		
+		ServletConfig config=getServletConfig();
+		String name=config.getInitParameter("name1");
+		String Email=config.getInitParameter("email1");
+		String City=config.getInitParameter("city1");
+		
+		
+		out.println("Name : "+name);
+		out.println("Email : "+Email);
+		out.println("City : "+City);
+		
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	service(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	service(req, resp);	
+	}
+
+}
